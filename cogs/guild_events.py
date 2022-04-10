7e057c24d1ad8e572b3949aac2a506cc2950dcee
PicklesWithSalt <93337490+PicklesWithSalt@users.noreply.github.com>
2022-04-10 17:23:07 +0200

    testing webhook

--- cogs/guild_events.py
+++ cogs/guild_events.py
@@ -10,7 +10,7 @@ class guild_events(commands.Cog):
     async def on_guild_join(self, guild):
         sys_channel = guild.system_channel
         if sys_channel is not None:
-            sup_embed = discord.Embed(title=f"Hi, I'm {self.bot.user}!", description=f"Thanks for inviting me! My prefix is `!`, but you can change it to whatever you want!\nUse `!help` to learn what I can do!", color=colors.default)
+            sup_embed = discord.Embed(title=f"Hi, I'm <@{self.bot.user.id}>!", description=f"Thanks for inviting me! My prefix is `!`, but you can change it to whatever you want!\nUse `!help` to learn what I can do!", color=colors.default)
             await sys_channel.send(embed=sup_embed)
 
 def setup(bot):
