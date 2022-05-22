c2ca6a1b95a6ca73fc8b79b46a97c481fcfdf133
PicklesWithSalt <93337490+PicklesWithSalt@users.noreply.github.com>
2022-05-22 09:23:00 +0200

    Make the say command good

--- Commands/say.js
+++ Commands/say.js
@@ -1,6 +1,7 @@
+const { colors } = require("../struc/colors");
+
 module.exports = {
   name: "say",
-  dm_permission: false,
   options: [
     {
       name: "message",
@@ -8,9 +9,21 @@ module.exports = {
       description: "The message to announce",
       required: true,
     },
+    {
+      name: "title",
+      type: 3,
+      description: "The title of the announcement",
+      required: true,
+    },
   ],
   description: "Repeats your message.",
   async run(bot, interaction, Eris) {
-    await interaction.createMessage(interaction.data.options[0].value);
+    say_embed = {
+      title: interaction.data.options[1].value,
+      description: interaction.data.options[0].value,
+      color: colors.default,
+    }
+
+    await interaction.createMessage({ embed: say_embed });
   },
 };
